3dfe830a4ffa554938bce7326d111ce1ca21a183
smilek <akstndizz@naver.com>
2017-01-13 12:38:37 +0900

    initial code

---
+++ Helloworld/src/com/test/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.test.helloworld;
+
+public class HelloWorld {
+	public static void main(String args[]){
+		
+		int value=1;//항상 소문자로, 대소문자 구분의 차이
+		System.out.println("Hello, World!");//가장 기본
+	}
+}
