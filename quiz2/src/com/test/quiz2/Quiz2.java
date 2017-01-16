1114579ad80b35acc06dfca01e1dd461c4389470
smilek <akstndizz@naver.com>
2017-01-16 10:05:07 +0900

    170116 09시 수업 Quiz2

---
+++ quiz2/src/com/test/quiz2/Quiz2.java
@@ -0,0 +1,47 @@
+package com.test.quiz2;
+
+import java.util.Scanner;
+
+public class Quiz2 {
+public static void main(String []args){
+	
+	System.out.println("숫자입력 : ");
+	
+	Scanner input = new Scanner(System.in);
+	
+	int value=input.nextInt();
+	
+	switch(value){
+		case 1 :
+			System.out.println("*");
+			System.out.println("**");
+			System.out.println("***");
+			System.out.println("****");
+			System.out.println("*****");
+			break;
+		case 2 :
+			System.out.println("    *");
+			System.out.println("   **");
+			System.out.println("  ***");
+			System.out.println(" ****");
+			System.out.println("*****");
+			break;
+		case 3 :
+			
+			System.out.println("*****");
+			System.out.println(" ****");
+			System.out.println("  ***");
+			System.out.println("   **");
+			System.out.println("    *");
+			break;
+		case 4 :
+			
+			System.out.println("*****");
+			System.out.println("****");
+			System.out.println("***");
+			System.out.println("**");
+			System.out.println("*");
+			break;
+	}
+	}
+}
