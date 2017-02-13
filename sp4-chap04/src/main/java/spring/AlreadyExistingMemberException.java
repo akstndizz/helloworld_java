59ff94def92051ecbefaaa2a57b8376b56a597ee
smilek <akstndizz@naver.com>
2017-02-13 12:53:46 +0900

    spring chap04

---
+++ sp4-chap04/src/main/java/spring/AlreadyExistingMemberException.java
@@ -0,0 +1,5 @@
+package spring;
+
+public class AlreadyExistingMemberException extends RuntimeException {
+	public AlreadyExistingMemberException(String message) { super(message);	}
+}
---
+++ sp4-chap04/src/main/java/spring/ChangePasswordService.java
@@ -0,0 +1,21 @@
+package spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ChangePasswordService {
+	private MemberDao memberDao;
+	
+	@Autowired
+	public ChangePasswordService(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+	
+	public void changePassword(String email, String oldPwd, String newPwd) {
+		Member member = memberDao.selectByEmail(email);
+		if(member==null)
+			throw new MemberNotFoundException();
+		member.changePassword(oldPwd, newPwd);
+		
+		memberDao.update(member);
+	}
+}
